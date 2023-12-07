f9ed4734df2ef0746ac5ba38ffc3d93f97b7bef1
xiaowanglon <129577432+xiaowanglon@users.noreply.github.com>
2023-12-07 17:15:44 +0800

    Add files via upload

---
+++ test_6_if_else.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 1;
+//	if (age !=0)
+//		printf("true");
+//	else
+//		printf("未执行");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 1;
+//	if (age != 0)
+//	{
+//		printf("true"); 
+//	 
+//		printf("true2");//surpass one need "{}"
+//	}
+//		
+//	else
+//	{
+//		printf("未执行");
+//
+//		printf("未执行");
+//	}
+//	return 0;
+//}
+
+////多分支语句
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 1;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if(age >=18&&age<28)
+//		printf("青年\n");
+//	else if(age>=28&&age<40)
+//		printf("中年\n");
+//	else if(age>40&&age<60)
+//		printf("壮年\n");
+//	else
+//		printf("老年\n");
+//}
+
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 1;
+//	if (age == 2)
+//		if (age == 1)
+//			printf("hh");
+//		else
+//			printf("ii");
+//	
+//}
+
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 100)
+	{
+			printf("%d ",i);
+		i+=2;//open your mind,it's great
+	}
+}
+
